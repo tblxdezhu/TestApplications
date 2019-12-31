1757e4e04fc941c54729c0303ef828aeeebbffc3
zhenxuan.xu <zhenxuan.xu@Zhenxuan.local>
2019-12-31 17:02:09 +0800

    change localhost:port

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn -b 0.0.0.0:80 app:app -w 3 --log-level=debug --access-logfile gunicorn.log $*
+gunicorn -b 127.0.0.1:9999 app:app -w 3 --log-level=debug --access-logfile gunicorn.log $*
