ce5043bc13f204f96fc1a86f052c88a2e6109d95
zhenxuan.xu <zhenxuan.xu@Zhenxuan.local>
2019-12-31 17:02:53 +0800

    mv kill to run

--- kill.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ps -ef |grep gunicorn |awk '{print "kill -9 "$2}'|sh
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+ps -ef |grep gunicorn |awk '{print "kill -9 "$2}'|sh
 gunicorn -b 127.0.0.1:9999 app:app -w 3 --log-level=debug --access-logfile gunicorn.log $*
